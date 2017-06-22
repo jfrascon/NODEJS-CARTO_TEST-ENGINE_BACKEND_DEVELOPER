860b177d3287f527535090f38014cea3e000f3e2
Raul Ochoa <rochoaf@gmail.com>
2017-06-22 15:56:50 +0200

    Resources, map viewer, and examples

---
+++ mapnik-example.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const fs = require('fs');
+const mapnik = require('mapnik');
+const path = require('path');
+
+mapnik.register_datasource(path.join(mapnik.settings.paths.input_plugins, 'shape.input'));
+
+const TILE_SIZE = 256;
+
+const map = new mapnik.Map(TILE_SIZE, TILE_SIZE);
+map.load('./style-admin0.xml', function(err, map) {
+    map.zoomAll();
+    const im = new mapnik.Image(TILE_SIZE, TILE_SIZE);
+    map.render(im, function(err, im) {
+        im.encode('png', function(err, buffer) {
+            fs.writeFile('map.png', buffer);
+         });
+    });
+});
